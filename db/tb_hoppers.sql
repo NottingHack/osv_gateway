e4d227eecdff4b51b0e9384e7c49eb79e6091857
James Hayward <jhayward1980@gmail.com>
2012-07-22 22:15:50 +0100

    Updated DB to include reserved stock info

--- db/tb_hoppers.sql
+++ db/tb_hoppers.sql
@@ -6,6 +6,7 @@ create table hoppers
 	hopper_id   int not null,-- 0 - 63, ID set on DIP switch on hopper, unique only within machine
 	product_id  int,
 	stock       int,
+	reserved    int,
 	
 	primary key (machine_id, hopper_id)
 ) ENGINE = InnoDB; 
