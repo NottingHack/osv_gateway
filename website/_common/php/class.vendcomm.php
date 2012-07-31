fb73cc052bf8bb5f6d5247306d11d7bc65f08728
James Hayward <jhayward1980@gmail.com>
2012-07-31 16:12:42 +0100

    Added dummy VendComm class
    This class communicates with the open websockets to the vending machines.

---
+++ website/_common/php/class.vendcomm.php
@@ -0,0 +1,20 @@
+<?php
+
+class VendComm {
+	
+	
+	public function __construct() {
+		
+	}
+	
+	public function checkStock($iMachineID, $iHopperID) {
+		return true;
+	}
+	
+	public function vend($iMachineID, $iHopperID) {
+		return true;
+	}
+	
+}
+
+?>
