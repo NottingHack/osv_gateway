c382d9681c0a1675cbc6a2c73e8a2895f12dac06
James Hayward <jhayward1980@gmail.com>
2012-08-18 17:59:47 +0100

    Started websocket code.
    Basic "hello world" completed, will allow connections from vending
    machine controllers, ask them their machine ID and status.
    
    Will need a full rewrite now to implement the OSV Websocket Protocol

---
+++ websocket/vend_control.php
@@ -0,0 +1,47 @@
+<?php
+require_once('websockets.php');
+
+class vendControl extends WebSocketServer {
+	//protected $maxBufferSize = 1048576; //1MB... overkill for an echo server, but potentially plausible for other applications.
+	
+	
+	
+	// Overload function for debugging
+	protected function send ($user, $message) {
+		$this->stdout("> $message");
+		parent::send($user, $message);
+	}
+	
+	protected function process ($user, $message) {
+		$this->stdout("< $message");
+		if (substr($message, 0, 9) == "MACHINEID") {
+			$this->send($user,"STATUS");
+		}
+		//$this->send($user,$message);
+	}
+	
+	protected function connected ($user) {
+		// Do nothing: This is just an echo server, there's no need to track the user.
+		// However, if we did care about the users, we would probably have a cookie to
+		// parse at this step, would be looking them up in permanent storage, etc.
+		$this->send($user, "MACHINEID");
+	}
+	
+	protected function closed ($user) {
+		// Do nothing: This is where cleanup would go, in case the user had any sort of
+		// open files or other objects associated with them.  This runs after the socket 
+		// has been closed, so there is no need to clean up the socket itself here.
+	}
+	
+	protected function processProtocol($protocol) {
+		return "Sec-WebSocket-Protocol: $protocol"; // return either "Sec-WebSocket-Protocol: SelectedProtocolFromClientList\r\n" or return an empty string.  
+				   // The carriage return/newline combo must appear at the end of a non-empty string, and must not
+				   // appear at the beginning of the string nor in an otherwise empty string, or it will be considered part of 
+				   // the response body, which will trigger an error in the client as it will not be formatted correctly.
+	}
+}
+
+$echo = new vendControl('10.0.0.5', 1402);
+
+
+?>
